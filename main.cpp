855d99f60267f7c780f3ecb2d0ee6b79f8fd0045
Антон Шмидт <schmidt.ajax@gmail.com>
2022-01-19 02:39:34 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -1,14 +1,47 @@
 #include <cassert>
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
+  template <class C>
+  void clearDigitsMemory(C& container) {
+    for (auto &line: container) {
+      fill(line.begin(), line.end(), false);
+    }
+  }
+
  public:
   bool isValidSudoku(vector<vector<char>> &board) {
-    // FIXME
-    return false;
+    constexpr int SIZE = 9;
+    array<array<bool, SIZE>, SIZE> rows;
+    array<array<bool, SIZE>, SIZE> columns;
+    array<array<bool, SIZE>, SIZE> squares;
+    clearDigitsMemory(rows);
+    clearDigitsMemory(columns);
+    clearDigitsMemory(squares);
+
+    for (int row = 0; row < board.size(); ++row) {
+      const auto &line = board[row];
+      for (int column = 0; column < line.size(); ++column) {
+        int digit = line[column] - 0x30;
+        if (0 < digit and digit < 10) {
+          int square_index = 3 * (row / 3) + column / 3;
+          const int digit_pos = digit - 1;
+          if (rows[row][digit_pos] || columns[column][digit_pos] || squares[square_index][digit_pos]) {
+            // digit repeated
+            return false;
+          }
+          rows[row][digit_pos] = true;
+          columns[column][digit_pos] = true;
+          squares[square_index][digit_pos] = true;
+        }
+      }
+    }
+
+    return true;
   }
 };
 
